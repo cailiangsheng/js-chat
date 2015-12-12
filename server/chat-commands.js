bb56c5d513009bda41f38e98c6c71c3cc50e5cf7
cailiangsheng <cailiangsheng@me.com>
2015-12-12 21:22:17 +0800

    fix chatting command

--- server/chat-commands.js
+++ server/chat-commands.js
@@ -9,28 +9,45 @@ var commands = {
 };
 
 module.exports = {
-    isCommand: getHandler,
+    isCommand: getCommand,
     execute: execute
 };
 
-function getHandler(message) {
-    var msg = message.trim().toLowerCase();
-    return _.find(commands, function (value, key) {
-        return msg.indexOf(key + ' ') == 0;
+function getRegExp(command) {
+    return new RegExp('^\\s*(\\' + command + ')\\s+(.*)\\s*$', 'i');
+}
+
+function getCommand(message) {
+    var command = null;
+    _.find(commands, function (value, key) {
+        var pattern = getRegExp(key);
+        var parts = message.match(pattern);
+        if (parts) {
+            command = {
+                name: parts[1],
+                text: parts[2],
+                handler: value
+            }
+            return true;
+        }
+        return false;
     });
+    return command;
 }
 
 function execute(fromSocket, message, timestamp) {
-    var handler = getHandler(message);
-    if (handler) {
-        var text = message.substr(5).trim();
-        handler(fromSocket, text, timestamp);
+    var command = getCommand(message);
+    if (command) {console.log(JSON.stringify(command));
+        var text = command.text;
+        command.handler(fromSocket, text, timestamp);
     }
 }
 
 function renameUser(fromSocket, text, timestamp) {
     var newName = text;
-    users.getUser(fromSocket).name = newName;
+    if (newName) {
+        users.getUser(fromSocket).name = newName;
+    }
 }
 
 function feedback(fromSocket, text, timestamp) {
@@ -43,5 +60,5 @@ function sendUserList(fromSocket, text, timestamp) {
 }
 
 function disconnect(fromSocket, text, timestamp) {
-    fromSocket.disconnect();
+    fromSocket.end();
 }
