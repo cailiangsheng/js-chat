19d06e939114be6e26e8e7b5a017c8bc1f022aa5
Larry Cai <larry.cai@sigma.namee>
2017-09-09 03:02:44 +0800

    use remote server

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ var chatLogger = require('./controller/chat-logger');
 var chatUI = require('./controller/chat-ui');
 var chatClient = require('./model/chat-client');
 
-init('localhost', 8888);
+init('ecs.zeta.name', 8888);
 
 function init(host, port) {
     chatLogger.enableLogging();
